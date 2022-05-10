2edfa6040ba30ce7d06fc72f51c9f657fd84b925
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-10 07:26:19 -0400

    validate store name to be atleast 3 characters

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -15,3 +15,4 @@ Employee.validates :last_name, presence: true
 Employee.validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
 Employee.validates :store_id, presence: true
 
+Store.validates :name, length: { minimum: 3 }
